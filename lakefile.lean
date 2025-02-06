9c5acde6173f28f1fb89e530bbf99cb9b3a5cd36
riyazahuja <riyaza@andrew.cmu.edu>
2025-02-05 23:49:50 -0500

    14

--- lakefile.lean
+++ lakefile.lean
@@ -10,7 +10,7 @@ lean_lib REPL {
 }
 
 require mathlib from git
-    "https://github.com/leanprover-community/mathlib4.git" @ "v4.15.0"
+    "https://github.com/leanprover-community/mathlib4.git" @ "v4.14.0"
 
 @[default_target]
 lean_exe repl where
